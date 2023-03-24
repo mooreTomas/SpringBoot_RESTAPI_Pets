e8bca49874339544e359f06bf064c9339d47af85
Thomas Moore <tommymoore@live.ie>
2023-03-24 20:38:12 +0000

    Task 2 now returns PDF, proper error handling

--- src/main/java/com/example/assignmenttwo_starter/controller/CustomerController.java
+++ src/main/java/com/example/assignmenttwo_starter/controller/CustomerController.java
@@ -1,6 +1,6 @@
 package com.example.assignmenttwo_starter.controller;
 
-import com.example.assignmenttwo_starter.exceptions.OrderNotFoundException;
+import com.example.assignmenttwo_starter.exceptions.InvoiceErrorException;
 
 
 import com.example.assignmenttwo_starter.model.*;
@@ -29,6 +29,7 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
 import org.springframework.core.annotation.Order;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
@@ -48,14 +49,14 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import org.apache.poi.xwpf.usermodel.XWPFDocument;
-import org.apache.poi.xwpf.usermodel.XWPFParagraph;
-import org.apache.poi.xwpf.usermodel.XWPFRun;
-import org.apache.poi.xwpf.usermodel.XWPFTable;
-import org.apache.poi.xwpf.usermodel.XWPFTableRow;
-import org.apache.poi.xwpf.usermodel.ParagraphAlignment;
 
-import javax.swing.text.html.Option;
+import com.itextpdf.io.font.constants.StandardFonts;
+import com.itextpdf.kernel.geom.PageSize;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.property.TextAlignment;
 
 
 @RestController
@@ -167,10 +168,6 @@ public class CustomerController {
     }
 
 
-
-
-
-
     // get all order to tests prior method
     @GetMapping(value = "/orders", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<List<Orders>> getAllOrders() {
@@ -186,8 +183,6 @@ public class CustomerController {
     }
 
 
-
-
     @DeleteMapping("/{customerId}")
     public ResponseEntity deleteCustomer(@PathVariable long customerId) {
         customerService.deleteById(customerId);
@@ -200,7 +195,7 @@ public class CustomerController {
         if (!c.isPresent()) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         } else {
-            Link link = linkTo(methodOn(CustomerController.class).getAllPagination(1, 10)).withSelfRel();
+            Link link = linkTo(methodOn(CustomerController.class).getAllPagination(0, 10)).withSelfRel();
             c.get().add(link);
             return ResponseEntity.ok(c.get());
         }
@@ -208,15 +203,19 @@ public class CustomerController {
 
     // id 34 has items and order information (for testing)
 
-    @GetMapping(value = "/invoice/{orderId}", produces  = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE} )
-    public ResponseEntity<String> generateInvoice(@PathVariable long orderId, @RequestHeader(value="Accept-Language", required=false) Locale locale) {
+
+    // "?" Response Entity in order to implement proper error handling, otherwise <ByteArrayResource>
+    @GetMapping(value = "/invoice/{orderId}", produces = {MediaType.APPLICATION_PDF_VALUE, MediaType.TEXT_PLAIN_VALUE})
+
+    public ResponseEntity<?> generateInvoice(@PathVariable long orderId, @RequestHeader(value = "Accept-Language", required = false) Locale locale) {
 
         // find order
         // check status (return bad request if not processing or pending)
         // then get customer and orderItem collection via the customer id in Orders and orderItemCollection in Orders
         // generate basic invoice format with gets from Orders and Customer
         // for loop to get the productId for each orderItem, use this to get Product name(s), quantity, price and total
-        // finally generate the footer and return the <String> ResponseEntity
+        // finally generate the footer and return the <ByteArrayResource> ResponseEntity
+
 
         Optional<Orders> optionalOrder = orderService.findById(orderId);
         if (optionalOrder.isEmpty()) {
@@ -225,52 +224,88 @@ public class CustomerController {
 
         Orders order = optionalOrder.get();
         OrderStatus status = order.getOrderStatusId();
+        List<OrderItem> items = order.getOrderItemCollection();
 
-        // Check if the order status is either "pending" or "processing"
-        if (!"pending".equals(status.getStatus().toLowerCase()) &&
-                !"processing".equals(status.getStatus().toLowerCase())) {
-            return ResponseEntity.badRequest().body("Invoice can only be generated for orders with status 'Pending' or 'Processing'");
-        } else {
+        try {
 
-            Customer customer = order.getCustomerId();
-            List<OrderItem> items = order.getOrderItemCollection();
+            // Check if the order status is either "pending" or "processing"
+            if (!"pending".equals(status.getStatus().toLowerCase()) &&
+                    !"processing".equals(status.getStatus().toLowerCase()) || items.isEmpty()) {
+                throw new InvoiceErrorException();
 
+            } else {
 
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern(messageSource.getMessage("date.format", null, locale));
-            String formattedDate = order.getOrderDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate().format(formatter);
+                Customer customer = order.getCustomerId();
 
 
-            // Generate the invoice header
-            String invoiceTitle = messageSource.getMessage("invoice.title", null, locale);
-            String invoice = String.format("%s:\n\n%s: %d\n%s: %s\n%s: %s %s\n\n",
-                    invoiceTitle, messageSource.getMessage("invoice.orderId", null, locale), order.getOrderId(),
-                    messageSource.getMessage("invoice.date", null, locale), formattedDate,
-                    messageSource.getMessage("invoice.customer", null, locale), customer.getFirstName(), customer.getLastName());
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern(messageSource.getMessage("date.format", null, locale));
+                String formattedDate = order.getOrderDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate().format(formatter);
 
-            // Generate the invoice body for each order item
-            BigDecimal grandTotal = BigDecimal.ZERO;
-            for (OrderItem item : items) {
-                Product product = item.getProductId();
-                BigDecimal itemTotal = item.getPrice().multiply(BigDecimal.valueOf(item.getQuantity()));
-                grandTotal = grandTotal.add(itemTotal);
+                try {
+                    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                    PdfWriter writer = new PdfWriter(outputStream);
+                    PdfDocument pdf = new PdfDocument(writer);
+                    Document document = new Document(pdf, PageSize.A4);
 
-                invoice += String.format("%s - %d x $%.2f = $%.2f\n",
-                        product.getName(), item.getQuantity(), item.getPrice(), itemTotal);
+                    // Generate the invoice header
+                    String invoiceTitle = messageSource.getMessage("invoice.title", null, locale);
+                    Paragraph title = new Paragraph(invoiceTitle)
+                            .setTextAlignment(TextAlignment.CENTER)
+                            .setBold()
+                            .setFontSize(18);
+                    document.add(title);
+
+                    // ... (existing code for creating the invoice body)
+
+                    String invoiceDetails = String.format("%s: %d\n%s: %s\n%s: %s %s\n%s: %s\n\n",
+                            messageSource.getMessage("invoice.orderId", null, locale), order.getOrderId(),
+                            messageSource.getMessage("invoice.date", null, locale), formattedDate,
+                            messageSource.getMessage("invoice.customer", null, locale), customer.getFirstName(), customer.getLastName(),
+                            messageSource.getMessage("invoice.address", null, locale), customer.getAddress());
+
+
+                    document.add(new Paragraph(invoiceDetails));
+
+                    // Generate the invoice body for each order item
+                    BigDecimal grandTotal = BigDecimal.ZERO;
+                    for (OrderItem item : items) {
+                        Product product = item.getProductId();
+                        BigDecimal itemTotal = item.getPrice().multiply(BigDecimal.valueOf(item.getQuantity()));
+                        grandTotal = grandTotal.add(itemTotal);
+
+                        String itemDetails = String.format("%s - %d x $%.2f = $%.2f\n",
+                                product.getName(), item.getQuantity(), item.getPrice(), itemTotal);
+                        document.add(new Paragraph(itemDetails));
+                    }
+
+                    // Generate the invoice footer with the grand total
+                    String localisedTotal = messageSource.getMessage("invoice.total", null, locale);
+                    Paragraph total = new Paragraph(String.format(localisedTotal + ": $%.2f", grandTotal))
+                            .setBold();
+                    document.add(total);
+
+                    String message = messageSource.getMessage("invoice.thanks", null, locale);
+                    Paragraph thanks = new Paragraph(message);
+                    document.add(thanks);
+
+                    document.close();
+
+                    HttpHeaders headers = new HttpHeaders();
+                    headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=invoice-" + orderId + ".pdf");
+
+                    return ResponseEntity.ok()
+                            .headers(headers)
+                            .contentType(MediaType.APPLICATION_PDF)
+                            .body(new ByteArrayResource(outputStream.toByteArray()));
+                } catch (Exception e) {
+                    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+                }
             }
-
-            // Generate the invoice footer with the grand total
-            String localisedTotal = messageSource.getMessage("invoice.total", null, locale);
-            invoice += String.format("\n" + localisedTotal +": $%.2f\n", grandTotal);
-
-
-
-            String message = messageSource.getMessage("invoice.thanks", null, locale);
-            invoice += message;
-
-
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.TEXT_PLAIN);
-            return new ResponseEntity<>(invoice, headers, HttpStatus.OK);
+        } catch (InvoiceErrorException ex) {
+            String errorMessage = messageSource.getMessage("invoice.error", null, locale);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .contentType(MediaType.TEXT_PLAIN)
+                    .body(errorMessage);
         }
     }
 
@@ -278,10 +313,7 @@ public class CustomerController {
 
 
 
-
-
-
-    @PutMapping("")
+        @PutMapping("")
     public ResponseEntity edit(@RequestBody Customer c) {
         customerService.saveCustomer(c);
         return new ResponseEntity<>(HttpStatus.OK);
